e3e701fd1911df82dadb16e670964a18388bb84f
Muthu Kumar <mk-dev@outlook.com>
2021-09-10 13:45:27 +0800

    basics-events-styles

--- conc/basics/app.js
+++ conc/basics/app.js
@@ -1,3 +1,5 @@
+// App for basics
+
 // const app = Vue.createApp({
 //   data() {
 //     return {
@@ -21,52 +23,100 @@
 
 // app.mount("#user-goal");
 
-const app = Vue.createApp({
+// App for events
+
+// const app = Vue.createApp({
+//   data() {
+//     return {
+//       counter: 10,
+//       name: "Muthu",
+//       age: 30,
+//       confirmedName: "",
+//       fullName: "",
+//     };
+//   },
+//   watch: {
+//     counter(value) {
+//       this.counter = value > 50 ? 0 : value;
+//     },
+//     name(value, prevVal) {
+//       this.fullName = value + " Kumar";
+//     },
+//   },
+//   computed: {
+//     // fullName() {
+//     //   console.log("fullName");
+//     //   return this.name + "Kumar";
+//     // },
+//   },
+//   methods: {
+//     confirmName() {
+//       this.confirmedName = this.name;
+//     },
+//     getModifiedName() {
+//       return "Some name";
+//     },
+//     increment() {
+//       console.log("increment");
+//       this.counter = this.counter + 1;
+//     },
+//     incrementByFive(inVal) {
+//       console.log("incrementByFive");
+//       this.counter = this.counter + inVal;
+//     },
+//     decrement() {
+//       this.counter--;
+//     },
+//     setName(e, lname) {
+//       this.name = e.target.value;
+//     },
+//     submitForm(event) {
+//       console.log("Submitted");
+//       // event.preventDefault();
+//     },
+//     reset() {
+//       this.name = "";
+//       this.age = "";
+//       this.confirmedName = "";
+//     },
+//   },
+// });
+
+// app.mount("#events");
+
+// App for styling
+
+const styleApp = {
   data() {
     return {
-      counter: 10,
-      name: "Muthu",
-      age: 30,
-      confirmedName: "",
+      title: "Vue Styles!",
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
     };
   },
   computed: {
-    fullName() {
-      console.log("fullName");
-      return this.name + "Kumar";
+    boxAClasses() {
+      return { active: this.boxASelected };
     },
   },
   methods: {
-    confirmName() {
-      this.confirmedName = this.name;
+    boxSelected(box) {
+      if (box === "A") {
+        this.boxASelected = !this.boxASelected;
+      } else if (box === "B") {
+        this.boxBSelected = !this.boxBSelected;
+      } else if (box === "C") {
+        this.boxCSelected = !this.boxCSelected;
+      }
     },
-    getModifiedName() {
-      return "Some name";
-    },
-    increment() {
-      console.log("increment");
-      this.counter = this.counter + 1;
-    },
-    incrementByFive(inVal) {
-      console.log("incrementByFive");
-      this.counter = this.counter + inVal;
-    },
-    decrement() {
-      this.counter--;
-    },
-    setName(e, lname) {
-      this.name = e.target.value;
-    },
-    submitForm(event) {
-      console.log("Submitted");
-      // event.preventDefault();
-    },
-    reset() {
-      this.name = "";
-      this.age = "";
-      this.confirmedName = "";
+    updateBoxAState() {
+      this.boxASelected = !this.boxCSelected;
+      this.boxBSelected = !this.boxASelected;
+      this.boxCSelected = !this.boxBSelected;
     },
   },
-});
+  watch: {},
+};
 
-app.mount("#events");
+Vue.createApp(styleApp).mount("#styling");
