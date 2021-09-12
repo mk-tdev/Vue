a07389cb61836153b6d9370fc56f405863275261
Muthu Kumar <mk-dev@outlook.com>
2021-09-12 18:45:41 +0800

    =forms

---
+++ forms-app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <the-form></the-form>
+</template>
+
+<script>
+import TheForm from "./components/TheForm.vue";
+
+export default {
+  name: "App",
+  components: {
+    TheForm,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+  background-color: #333;
+  color: white;
+}
+</style>
---
+++ forms-app/src/components/TheForm.vue
@@ -0,0 +1,193 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="user-name">Your Name</label>
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="formName"
+      />
+    </div>
+    <div class="form-control">
+      <label for="age">Your Age (Years)</label>
+      <input id="age" name="age" type="number" v-model.number="formAge" />
+    </div>
+    <div class="form-control">
+      <label for="referrer">How did you hear about us?</label>
+      <select id="referrer" name="referrer" v-model="formWhere">
+        <option value="google">Google</option>
+        <option value="wom">Word of mouth</option>
+        <option value="newspaper">Newspaper</option>
+      </select>
+    </div>
+    <div class="form-control">
+      <h2>What are you interested in?</h2>
+      <div>
+        <input
+          id="interest-news"
+          name="interest"
+          type="checkbox"
+          value="news"
+          v-model="formWhat"
+        />
+        <label for="interest-news">News</label>
+      </div>
+      <div>
+        <input
+          id="interest-tutorials"
+          name="interest"
+          type="checkbox"
+          value="tutorials"
+          v-model="formWhat"
+        />
+        <label for="interest-tutorials">Tutorials</label>
+      </div>
+      <div>
+        <input
+          id="interest-nothing"
+          name="interest"
+          type="checkbox"
+          value="nothing"
+          v-model="formWhat"
+        />
+        <label for="interest-nothing">Nothing</label>
+      </div>
+    </div>
+    <div class="form-control">
+      <h2>How do you learn?</h2>
+      <div>
+        <input
+          id="how-video"
+          name="how"
+          type="radio"
+          value="video"
+          v-model="formHow"
+        />
+        <label for="how-video">Video Courses</label>
+      </div>
+      <div>
+        <input
+          id="how-blogs"
+          name="how"
+          type="radio"
+          value="blogs"
+          v-model="formHow"
+        />
+        <label for="how-blogs">Blogs</label>
+      </div>
+      <div>
+        <input
+          id="how-other"
+          name="how"
+          type="radio"
+          value="other"
+          v-model="formHow"
+        />
+        <label for="how-other">Other</label>
+      </div>
+    </div>
+    <div class="form-control">
+      <input
+        type="checkbox"
+        name="confirm-terms"
+        id="confirm-terms"
+        v-model="confirm"
+      />
+      <label for="confirm-terms">Agree to terms, Confirm?</label>
+    </div>
+    <div>
+      <button>Save Data</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formName: "",
+      formAge: null,
+      formWhere: "google",
+      formWhat: [],
+      formHow: null,
+      confirm: false,
+    };
+  },
+  methods: {
+    submitForm() {
+      this.formName = "";
+      this.formAge = null;
+      this.formWhere = "google";
+      this.formWhat = [];
+      this.formHow = null;
+      this.confirm = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 5px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  background-color: #ffffff;
+  color: #000;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+}
+
+h2 {
+  font-size: 1rem;
+  margin: 0.5rem 0;
+}
+
+input,
+select {
+  display: block;
+  width: 100%;
+  font: inherit;
+  margin-top: 0.5rem;
+}
+
+select {
+  width: auto;
+}
+
+input[type="checkbox"],
+input[type="radio"] {
+  display: inline-block;
+  width: auto;
+  margin-right: 1rem;
+}
+
+input[type="checkbox"] + label,
+input[type="radio"] + label {
+  font-weight: normal;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #0076bb;
+  background-color: #0076bb;
+  color: white;
+  cursor: pointer;
+  padding: 0.75rem 1rem;
+  border-radius: 5px;
+}
+
+button:hover,
+button:active {
+  border-color: #002350;
+  background-color: #002350;
+}
+</style>
